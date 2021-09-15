2e9c3763e350ad2463f9b153bf5f60dc4c66d8be
Satria Hadi <satria78.sh@gmail.com>
2021-09-15 18:17:25 +0700

    Added AutoMapper model to dto

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CommandAPI.Data;
 using CommandAPI.Models;
+using CommandAPI.Dtos;
+using AutoMapper;
 
 namespace CommandAPI.Controllers
 {
@@ -12,23 +14,26 @@ namespace CommandAPI.Controllers
         // Add the following code to our class
         private readonly ICommandAPIRepo _repository;
 
-        public CommandsController(ICommandAPIRepo repository)
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandAPIRepo repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         //Add the following code
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
         //Add the following code for our second ActionResult
         [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id)
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
 
@@ -37,7 +42,7 @@ namespace CommandAPI.Controllers
                 return NotFound();
             }
 
-            return Ok(commandItem);
+            return Ok(_mapper.Map<CommandReadDto>(commandItem));
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,7 @@ using CommandAPI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
 
 namespace CommandAPI
 {
@@ -38,6 +39,7 @@ namespace CommandAPI
             // SECTION 1. Add code below
             services.AddControllers();
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             /* services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>(); */
 
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
